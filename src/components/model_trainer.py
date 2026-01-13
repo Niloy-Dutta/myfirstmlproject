bf10df38425ae4c058829f4f68d0521d355d9fce
Niloy Dutta Anik <niloydutta2525@gmail.com>
2026-01-13 20:54:28 +0600

    Hyperparameter Tunning

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -18,6 +18,7 @@ from src.exception import CustomException
 from src.logger import logging
 from src.utils import save_object,evaluate_model
 
+
 @dataclass 
 class ModelTrainerConfig:
     trained_model_file_path = os.path.join('artifact','model.pkl')
@@ -45,9 +46,45 @@ class ModelTrainer:
                         "CatBoosting Regressor": CatBoostRegressor(verbose=False),
                         "AdaBoost Regressor": AdaBoostRegressor(),
             }
+                    params={
+                "Decision Tree": {
+                    'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                    # 'splitter':['best','random'],
+                    # 'max_features':['sqrt','log2'],
+                },
+                "Random Forest":{
+                    # 'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                 
+                    # 'max_features':['sqrt','log2',None],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "Gradient Boosting":{
+                    # 'loss':['squared_error', 'huber', 'absolute_error', 'quantile'],
+                    'learning_rate':[.1,.01,.05,.001],
+                    'subsample':[0.6,0.7,0.75,0.8,0.85,0.9],
+                    # 'criterion':['squared_error', 'friedman_mse'],
+                    # 'max_features':['auto','sqrt','log2'],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "Linear Regression":{},
+                "XGBRegressor":{
+                    'learning_rate':[.1,.01,.05,.001],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "CatBoosting Regressor":{
+                    'depth': [6,8,10],
+                    'learning_rate': [0.01, 0.05, 0.1],
+                    'iterations': [30, 50, 100]
+                },
+                "AdaBoost Regressor":{
+                    'learning_rate':[.1,.01,0.5,.001],
+                    # 'loss':['linear','square','exponential'],
+                    'n_estimators': [8,16,32,64,128,256]
+                }
+                  }
                     model_report:dict = evaluate_model(X_train= X_train,y_train= y_train,
                                                        X_test = X_test,y_test = y_test,
-                                                       models = models)
+                                                       models = models,params = params)
                     
                     # To get best name of a model 
                     best_model_name = max(model_report,key = model_report.get)
@@ -55,6 +92,7 @@ class ModelTrainer:
                     best_model_score = model_report[best_model_name]
 
                     best_model = models[best_model_name]
+                    
                     if best_model_score <.60:
                           logging.info("No best model found")
                     logging.info('Best found model on both training and testing dataset')
--- src/utils.py
+++ src/utils.py
@@ -5,6 +5,7 @@ import pandas as pd
 import numpy as np
 from src.exception import CustomException
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
 def save_object(file_path,obj):
     try: 
@@ -17,12 +18,19 @@ def save_object(file_path,obj):
     except Exception as e:
         raise CustomException(e,sys)
 
-def evaluate_model(X_train,y_train,X_test,y_test,models):
+def evaluate_model(X_train,y_train,X_test,y_test,models,params):
     try: 
         report = {}
         for i in range(len(list(models))):
             model = list(models.values())[i]
-            model.fit(X_train,y_train) # trianing the model
+            param = list(params.values())[i]
+            
+            grid_search = GridSearchCV(estimator=model,param_grid=param,n_jobs=-1)
+            grid_search.fit(X_train,y_train)
+            
+            model.set_params(**grid_search.best_params_)
+            model.fit(X_train,y_train) # trianing the model with updated hyperparameter
+        
             
             y_train_pred = model.predict(X_train)
             y_test_pred = model.predict(X_test)
